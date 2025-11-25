cc7f2750fe8b2176aa7314253f01627becda8ab5
sahil shukla <sahilshukla@gmail.com>
2025-11-25 17:37:34 +0530

    Initial commit - Prescripto project

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`);
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 export default connectDB;
+
